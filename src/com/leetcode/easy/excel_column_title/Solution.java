ffda1e975040d5a539fcf5eaf195f2097a4f1057
craig <cgroves06@gmail.com>
2019-07-01 23:16:47 -0400

    Still working on solution

--- src/com/leetcode/easy/excel_column_title/Solution.java
+++ src/com/leetcode/easy/excel_column_title/Solution.java
@@ -13,21 +13,31 @@ public class Solution {
         int lengthOfAlphabet = 26;
         int count = 0;
         StringBuilder builder = new StringBuilder();
-        if (number > lengthOfAlphabet)
+        int remainder = number % lengthOfAlphabet;
+        if (number > lengthOfAlphabet) {
             digits.add(count);
-        while (number > lengthOfAlphabet) {
-            number -= lengthOfAlphabet;
-            digits.set(digits.size()-1, digits.get(digits.size()-1) + 1);
-            if (digits.get(digits.size()-1) > lengthOfAlphabet) {
-                digits.set(digits.size()-1, 1);
-                digits.add(count);
+            while ((number / lengthOfAlphabet) > 0) {
+//            number -= lengthOfAlphabet;
+                number = number / lengthOfAlphabet;
+//            Math.log(number) / Math.log(lengthOfAlphabet);
+                digits.set(digits.size() - 1, digits.get(digits.size() - 1) + 1);
+                if (digits.get(digits.size() - 1) > lengthOfAlphabet) {
+                    digits.set(digits.size() - 1, 1);
+                    digits.add(1);
+                }
             }
         }
         for (int i = digits.size()-1; i >= 0; i--) {
             if (digits.get(i) > 0)
                 builder.append((char)(digits.get(i) - 1 + 'A'));
         }
-        builder.append((char)(number -1 + 'A'));
+
+        if (remainder > 0) {
+            builder.append((char) ((remainder) - 1 + 'A'));
+        }
+        else {
+            builder.append('Z');
+        }
         return builder.toString();
     }
 }
--- src/com/leetcode/easy/excel_column_title/main.java
+++ src/com/leetcode/easy/excel_column_title/main.java
@@ -3,10 +3,12 @@ package com.leetcode.easy.excel_column_title;
 public class main {
     public static void main(String[] args) {
         Solution solution = new Solution();
-//        System.out.println(solution.convertToTitle(52));
-//        System.out.println(solution.convertToTitle(53));
+        System.out.println(solution.convertToTitle(1));
+        System.out.println(solution.convertToTitle(28));
+        System.out.println(solution.convertToTitle(52));
+        System.out.println(solution.convertToTitle(53));
 //        System.out.println(solution.convertToTitle(626));
 //        System.out.println(solution.convertToTitle(701));
-        System.out.println(solution.convertToTitle(703));
+        System.out.println(solution.convertToTitle((int)Math.pow(26,3)));
     }
 }
